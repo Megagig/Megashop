1b5d17317d5a74f557e22117b88f912ed32c472a
Megagig <megagigsoftwaresolutions.com>
2024-04-22 18:38:07 +0100

    import fontawesome from cdnjs.com into the header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,10 +8,18 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="/">Megashop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                {' '}
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
