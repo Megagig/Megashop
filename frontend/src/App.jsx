0837ef79ba007748bf59bb80a488150ebb037ee1
Megagig <megagigsoftwaresolutions.com>
2024-04-22 16:50:32 +0100

    Import Boostrap Theme from (https://bootswatch.com)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <>
-      <h1>Welcome to Megashop</h1>
+      <Header />
+      <main>
+        <h1>Welcome to Megashop</h1>
+      </main>
+      <Footer />
     </>
   );
 };
