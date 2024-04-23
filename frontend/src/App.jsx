f2e3b1dca40fc8b95e4f496cf09882caa0e505e6
Megagig <megagigsoftwaresolutions.com>
2024-04-23 17:59:14 +0100

    Implememnt the react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen';
+
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
