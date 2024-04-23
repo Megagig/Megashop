d73df8cce22ef3f0f51e4a6230eea1bc70fc2fd6
Megagig <megagigsoftwaresolutions.com>
2024-04-23 16:32:43 +0100

    Implement the HomeScreen and the Product Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Megashop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
