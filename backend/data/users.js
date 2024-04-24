2bf623e8b1b4e9d9f161313b4d83e6efef3bcdb2
Megagig <megagigsoftwaresolutions.com>
2024-04-24 19:36:22 +0100

    Seeding Sample Data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John doe',
+    email: 'john@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Jane doe',
+    email: 'jane@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
