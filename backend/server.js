57c228fafc67a54810e4c49eb2f8bf63c512fe29
Megagig <megagigsoftwaresolutions.com>
2024-04-24 13:54:38 +0100

    Express setup with nodemon & Concurrently

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import products from './data/products.js';
+const port = 5000;
+const app = express(); //initialize express
+app.get('/', (req, res) => {
+  res.send('API is Running....');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send(products); // get all products
+});
+
+// get the products by id
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ msg: 'Product Not Found' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
